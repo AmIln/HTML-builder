37f4b9c1fa3e16be0d8c6ca6e30491831082ade0
Dmitry Andriyanov <facklak@mail.ru>
2024-01-19 17:23:53 +0300

    refactor: use stat instead of statSync

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,14 +5,14 @@ fs.readdir("03-files-in-folder/secret-folder", (error, files) => {
         if (error) return console.log(error);
 
         for (file of files) {
-            const ziseFile = `03-files-in-folder/secret-folder/${file}`,
-            stats = fs.statSync(ziseFile),
-            sizeButes = stats.size,
+            const pathFile = `03-files-in-folder/secret-folder/${file}`,
             nameFile = path.basename(file, path.extname(file)),
             typeFile = path.extname(file).replace('.', '');
-
-            if (stats.isFile()) {
-                console.log(`${nameFile} - ${typeFile} - ${sizeButes}b`);
-            }
+            ziseFile = fs.stat(pathFile, (err, stats) => {
+                if (err) { throw err };
+                if (stats.isFile()) {
+                    console.log(`${nameFile} - ${typeFile} - ${stats.size}b`);
+                }
+            });
         }
 });
