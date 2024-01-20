50397618090ba1486d4031f971191cb185896537
Dmitry Andriyanov <facklak@mail.ru>
2024-01-20 14:45:55 +0300

    feat: did 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const source = '04-copy-directory/files';
+const destination = `\\04-copy-directory\\copy-files`;
+
+function copyDir() {
+    fs.stat(path.resolve() + destination, (err) => {
+        // если папка есть
+        if (!err) {
+            fs.readdir(source, (err, files) => {
+                for (file of files) {
+                    fs.copyFile(source + '\\' + file, path.resolve() + destination + '\\' + file, err => {
+                        if(err) { throw err };
+                    });
+                }
+            })
+        } else {
+            fs.mkdir(path.resolve() + destination, (err) => {
+                if (err) { throw err };
+            });
+            fs.readdir(source, (err, files) => {
+                for (file of files) {
+                    fs.copyFile(source + '\\' + file, path.resolve() + destination + '\\' + file, err => {
+                        if(err) { throw err };
+                    });
+                }
+            })
+        }
+    })
+}
+copyDir()
